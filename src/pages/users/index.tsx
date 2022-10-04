aa2901d5fa2638e8b1ab31d0749fbefb9c0816ac
Andrew Kamiji Dias <andrew_kamiji_dias@hotmail.com>
2022-10-04 13:24:26 -0300

    Configurando React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,94 +1,116 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Th, Thead, Tr, Td, Text, useBreakpointValue } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Table,
+  Tbody,
+  Th,
+  Thead,
+  Tr,
+  Td,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
+import { useEffect } from "react";
 
 import { Pagination } from "../../components/Pagination/";
 import Link from "next/link";
 
 export default function UserList() {
-    const isWideVersion = useBreakpointValue({
-        base: false,
-        lg: true,
-    })
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
 
-    return (
-        <Box>
-            <Header />
+  useEffect(() => {
+    fetch("http://localhost:3000/api/users")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
 
-            <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
-                <Sidebar />
+  return (
+    <Box>
+      <Header />
 
-                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
-                    <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal">
-                            Usuários
-                        </Heading>
-                        <Link href="/users/create" passHref>
-                            <Button
-                                as="a"
-                                size="sm"
-                                fontSize="sm"
-                                colorScheme="pink"
-                            >
-                                Criar novo usuário
-                            </Button>
-                        </Link>
-                    </Flex>
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
 
-                    <Table colorScheme="whiteAlpha">
-                        <Thead>
-                            <Tr>
-                                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                                    <Checkbox colorScheme="pink" />
-                                </Th>
-                                <Th>Usuário</Th>
-                                {isWideVersion && <Th>Data de cadastro</Th>}
-                                <Th width="8"></Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Andrew Kamiji</Text>
-                                        <Text fontSize="sm" color="gray.300">kamiji@mail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>04 de Setembro, 2022</Td>}
-                            </Tr>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Andrew Kamiji</Text>
-                                        <Text fontSize="sm" color="gray.300">kamiji@mail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>04 de Setembro, 2022</Td>}
-                            </Tr>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Andrew Kamiji</Text>
-                                        <Text fontSize="sm" color="gray.300">kamiji@mail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>04 de Setembro, 2022</Td>}
-                            </Tr>
-                        </Tbody>
-                    </Table>
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+            <Link href="/users/create" passHref>
+              <Button as="a" size="sm" fontSize="sm" colorScheme="pink">
+                Criar novo usuário
+              </Button>
+            </Link>
+          </Flex>
 
-                    <Pagination />
-                </Box>
-            </Flex>
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                {isWideVersion && <Th>Data de cadastro</Th>}
+                <Th width="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px={["4", "4", "6"]}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Andrew Kamiji</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      kamiji@mail.com
+                    </Text>
+                  </Box>
+                </Td>
+                {isWideVersion && <Td>04 de Setembro, 2022</Td>}
+              </Tr>
+              <Tr>
+                <Td px={["4", "4", "6"]}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Andrew Kamiji</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      kamiji@mail.com
+                    </Text>
+                  </Box>
+                </Td>
+                {isWideVersion && <Td>04 de Setembro, 2022</Td>}
+              </Tr>
+              <Tr>
+                <Td px={["4", "4", "6"]}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Andrew Kamiji</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      kamiji@mail.com
+                    </Text>
+                  </Box>
+                </Td>
+                {isWideVersion && <Td>04 de Setembro, 2022</Td>}
+              </Tr>
+            </Tbody>
+          </Table>
+
+          <Pagination />
         </Box>
-    );
+      </Flex>
+    </Box>
+  );
 }
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,28 +1,46 @@
-import {  createServer, Model } from 'miragejs'
+import { createServer, Factory, Model } from "miragejs";
+import faker from "faker";
 
 type User = {
-    name: string;
-    email: string;
-    created_at: string;
+  name: string;
+  email: string;
+  created_at: string;
 };
 
 export function makeServer() {
-    const server = createServer({
-        models: {
-            user: Model.extend<Partial<User>>({})
+  const server = createServer({
+    models: {
+      user: Model.extend<Partial<User>>({}),
+    },
+    factories: {
+      user: Factory.extend({
+        name(i) {
+          return `User ${i + 1}`;
         },
+        email() {
+          return faker.internet.email().toLowerCase();
+        },
+        createdAt() {
+          return faker.date.recent(10);
+        },
+      }),
+    },
 
-        routes() {
-            this.namespace = 'api'
-            this.timing = 750;
+    seeds(server) {
+        server.createList("user", 200);
+    },
+    
+    routes() {
+      this.namespace = "api";
+      this.timing = 750;
 
-            this.get('/users');
-            this.post('/users');
+      this.get("/users");
+      this.post("/users");
 
-            this.namespace = '';
-            this.passthrough();
-        }
-    })
+      this.namespace = "";
+      this.passthrough();
+    },
+  });
 
-    return server;
+  return server;
 }
