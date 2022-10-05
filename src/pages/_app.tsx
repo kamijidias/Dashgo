06e2830da77880f323e3caf81ebaad71d34a5564
Andrew Kamiji Dias <andrew_kamiji_dias@hotmail.com>
2022-10-05 09:09:47 -0300

    continuando a estruturação do react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,27 @@
-import { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react'
-import { theme } from './styles/theme';
+import { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "./styles/theme";
 
-import { SideDrawerProvider } from '../contexts/SidebarDrawerContext';
-import { makeServer } from '../services/mirage';
+import { SideDrawerProvider } from "../contexts/SidebarDrawerContext";
+import { makeServer } from "../services/mirage";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   makeServer();
 }
 
+const queryClient = new QueryClient()
+
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <ChakraProvider theme={theme}>
-      <SideDrawerProvider>
-      <Component {...pageProps} />
-      </SideDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SideDrawerProvider>
+          <Component {...pageProps} />
+        </SideDrawerProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,25 +12,27 @@ import {
   Td,
   Text,
   useBreakpointValue,
+  Spinner,
 } from "@chakra-ui/react";
+import Link from "next/link";
+import { useQuery } from "react-query";
+
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
-import { useEffect } from "react";
-
 import { Pagination } from "../../components/Pagination/";
-import Link from "next/link";
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery("users", async () => {
+    const response = await fetch("http://localhost:3000/api/users");
+    const data = await response.json();
+
+    return data;
+  });
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
-
-  useEffect(() => {
-    fetch("http://localhost:3000/api/users")
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  }, []);
 
   return (
     <Box>
@@ -51,64 +53,75 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-                <Th width="8"></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Andrew Kamiji</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      kamiji@mail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de Setembro, 2022</Td>}
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Andrew Kamiji</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      kamiji@mail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de Setembro, 2022</Td>}
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Andrew Kamiji</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      kamiji@mail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de Setembro, 2022</Td>}
-              </Tr>
-            </Tbody>
-          </Table>
-
-          <Pagination />
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados do usuário</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                    <Th width="8"></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Andrew Kamiji</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          kamiji@mail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>04 de Setembro, 2022</Td>}
+                  </Tr>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Andrew Kamiji</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          kamiji@mail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>04 de Setembro, 2022</Td>}
+                  </Tr>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Andrew Kamiji</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          kamiji@mail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>04 de Setembro, 2022</Td>}
+                  </Tr>
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
